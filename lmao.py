e8173de44ffe4cac35ed9af54118ca66e5baf0b8
AGawas <arnavgawas707@gmail.com>
2026-02-26 19:58:06 -0500

    Update lmao.py

--- lmao.py
+++ lmao.py
@@ -1,5 +1,30 @@
 import serial
 
+ser = serial.Serial("/dev/pts/6", 9600)
+
+responses = {
+    'C': "Carry: set if there was a carry out of or no borrow into the msb.",
+    'N': "Negative: set if result is negative for signed number; N = msb.",
+    'V': "Overflow: set if result is invalid for signed number.",
+    'Z': "Zero: set if result is zero.",
+}
+
+def prompt():
+    ser.write(b"Type a letter for APSR glossary lookup.\r\n>")
+
+prompt()
+
+while True:
+    ch = ser.read(1).decode()
+    upper = ch.upper()
+
+    if upper in responses:
+        ser.write(ch.encode())  # echo original
+        ser.write(b"\r\n")
+        ser.write(responses[upper].encode())
+        ser.write(b"\r\n")
+        prompt()import serial
+
 ser = serial.Serial("COM2", 9600)
 
 responses = {
